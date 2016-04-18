b3b3985ed5872ed5f2713d8776249bf371746113
Ionică Bizău <bizauionica@gmail.com>
2016-04-18 10:16:08 +0300

    Examples

--- example/index.js
+++ example/index.js
@@ -2,4 +2,27 @@
 
 const searchFileInDirs = require("../lib");
 
-console.log(searchFileInDirs());
+// .
+// ├── bar
+// │   └── a
+// └── foo
+//     ├── a
+//     └── b
+let dirs = [
+    `${__dirname}/bar`
+  , `${__dirname}/foo`
+];
+
+// Search the file "b" in these dirs
+searchFileInDirs(dirs, "b", (err, path) => {
+    console.log(err || path);
+    // /home/.../example/foo/b
+});
+
+// Search file "a" in these dirs (bar has priority)
+searchFileInDirs(dirs, "a", (err, path, res) => {
+    console.log(err || path, res);
+    // /home/.../example/bar/a
+    // { '/home/.../example/bar/a': true,
+    //   '/home/.../example/foo/a': true }
+});
