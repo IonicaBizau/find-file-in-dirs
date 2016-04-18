4a9cc06536d789ae5995219308bfe4657ed6b5a7
Ionică Bizău <bizauionica@gmail.com>
2016-04-18 10:18:23 +0300

    Updated the name of the project

--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
 "use strict";
 
-const search = require("..")
+const find = require("..")
     , tester = require("tester")
     ;
 
-tester.describe("search", test => {
+tester.describe("find", test => {
     let dirs = [
         `${__dirname}/bar`
       , `${__dirname}/foo`
     ];
     test.should("find the file in one of dirs", cb => {
-        // Search the file "b" in these dirs
-        search(dirs, "b", (err, path) => {
+        // find the file "b" in these dirs
+        find(dirs, "b", (err, path) => {
             test.expect(err).toBe(null);
             test.expect(path.split("/").slice(-2).join("/")).toBe("foo/b");
             cb();
         });
     });
     test.should("find the file in both dirs", cb => {
-        search(dirs, "a", (err, path, res) => {
+        find(dirs, "a", (err, path, res) => {
             test.expect(err).toBe(null);
             test.expect(path.split("/").slice(-2).join("/")).toBe("bar/a");
             cb();
